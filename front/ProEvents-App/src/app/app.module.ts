913eafe14a267f79a193037ab22eb8fc81fd4449
Mayardes Oliveira <mayardesoliveira@gmail.com>
2023-05-09 21:22:29 -0300

    adding bootstrap, httpclient, browser animation

--- front/ProEvents-App/src/app/app.module.ts
+++ front/ProEvents-App/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakerComponent } from './speaker/speaker.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    SpeakerComponent
+    SpeakerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/ProEvents-App/src/app/events/events.component.ts
+++ front/ProEvents-App/src/app/events/events.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,25 +9,37 @@ import { Component } from '@angular/core';
 export class EventsComponent {
 
   //It's a object.
-  public events: any = [
-    {
-      Theme: 'Angular 16',
-      Local: 'Belo Horizonte' 
-    },
-    {
-      Theme: '.Net 7',
-      Local: 'São Paulo' 
-    },
-    {
-      Theme: 'Angular e suas novidades',
-      Local: 'Rio de Janeiro' 
-    }
-  ]
+  public events: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEventsHttp();
+  }
 
+
+  public getEvents(): void {
+    this.events =
+      [
+        {
+          Theme: 'Angular 16',
+          Local: 'Belo Horizonte'
+        },
+        {
+          Theme: '.Net',
+          Local: 'Manaus'
+        },
+        {
+          Theme: 'Angular and news',
+          Local: 'Rio de Janeiro'
+        }
+      ]
+  }
+
+  public getEventsHttp(): void {
+
+    this.http.get('http://localhost:5259/api/event')
+      .subscribe(response => this.events = response, error => console.log(error));
   }
 
 }
